7ac47d30ebc3b284308c24826e06235c7e400017
Jhon Rey <jhonreymendiola@gmail.com>
2023-07-24 16:29:14 +0800

    working login in the main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'security/login', pathMatch: 'full' },
   { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
-  // { path: 'login', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule) },
+  { path: 'main', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule) },
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
+    // CommonModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', redirectTo: '/main', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      // { path: 'search-patient-id', component: SearchPatientIdComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
