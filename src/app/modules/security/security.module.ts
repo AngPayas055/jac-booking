9fac4ef8e77e87c0561e24f44c4e89fa79b42854
Jhon Rey <jhonreymendiola@gmail.com>
2023-07-28 14:11:54 +0800

    toaster message

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -11,6 +11,7 @@ import { DropdownModule } from 'primeng/dropdown';
 import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HttpClientModule } from '@angular/common/http';
     DropdownModule,
     CalendarModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ToastModule
   ]
 })
 export class SecurityModule { }
